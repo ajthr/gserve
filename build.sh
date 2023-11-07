f5ab17f1f50e5339db32a9fd8099bef17915184b
Ajith <ajithar204@gmail.com>
2023-11-07 18:26:43 +0530

    add bash script to build package

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+package="github.com/ajthr/gserve"
+package_split=(${package//\// })
+package_name=${package_split[-1]}
+package_version="0.1"
+
+platforms=("linux/amd64" "windows/amd64" "darwin/amd64")
+
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name=$package_name'-'$package_version'-'$GOOS'-'$GOARCH
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi	
+
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
